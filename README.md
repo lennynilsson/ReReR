96c4ce1edf474481067f0fda95e2069123963dd5
Lenny Nilsson <nilsson.lenny@gmail.com>
2014-11-19 00:53:37 +0100

    Added Initializr

---
+++ README.md
@@ -0,0 +1,4 @@
+ReReR
+=====
+
+Reddit Reader - A static one page application to view images on Reddit
---
+++ js/main.js
@@ -0,0 +1,9 @@
+var R = function() {
+	 login : function() {
+    		 
+	 }
+};
+
+$(function() {
+	
+});
